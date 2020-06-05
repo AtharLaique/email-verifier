9c99b2772bdaf83b493ebbd316ef21a0e0701da7
Athar_Laique <athar@softcircles.org>
2020-06-05 19:02:58 +0500

    updation

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require("express");
 const app = express();
 const emailRoutes=require('./routes/email/email');
 app.use('/email-verifier',emailRoutes)
-app.listen(3000, () => {console.log("Server is running")});
+app.listen(3001, () => {console.log("Server is running")});
--- routes/email/email.js
+++ routes/email/email.js
@@ -4,9 +4,9 @@ route.get("/", (req, res) => {
   console.log();
   filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   if (filter.test(req.query.email)) {
-    return res.status(200).json({ valid: true });
+    return res.status(200).json({email:req.query.email,isValid:true});
   } else {
-    return res.status(400).json({ valid: false });
+    return res.status(200).json({ valid: false});
   }
 });
 module.exports = route;
